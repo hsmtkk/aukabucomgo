b9b66e7bf10f8106789fa3fd2a21b54d495e6efd
Alice <you@example.com>
2024-04-02 09:40:17 +0900

    update positionsget to use enum

--- info/positionsget/positionsget.go
+++ info/positionsget/positionsget.go
@@ -1,13 +1,11 @@
 package positionsget
 
 import (
-	"strconv"
-
 	"github.com/hsmtkk/aukabucomgo/base"
 )
 
 type Client interface {
-	PositionsGet(product int, side Side) ([]byte, error)
+	PositionsGet(product Product, side Side) ([]byte, error)
 }
 
 func New(baseClient base.Client) Client {
@@ -18,19 +16,36 @@ type clientImpl struct {
 	baseClient base.Client
 }
 
+type Product int
+
+const (
+	ALL    Product = iota // 0
+	FUTURE                // 3
+	OPTION                // 4
+)
+
 type Side int
 
 const (
-	BUY Side = iota
-	SELL
+	BUY  Side = iota // 1
+	SELL             // 2
 )
 
-func (clt *clientImpl) PositionsGet(product int, side Side) ([]byte, error) {
+func (clt *clientImpl) PositionsGet(product Product, side Side) ([]byte, error) {
 	sideStr := "undef"
 	if side == BUY {
 		sideStr = "1"
 	} else if side == SELL {
 		sideStr = "2"
 	}
-	return clt.baseClient.Get("/positions", map[string]string{"product": strconv.Itoa(product), "side": sideStr})
+	productStr := "undef"
+	switch product {
+	case ALL:
+		productStr = "0"
+	case FUTURE:
+		productStr = "3"
+	case OPTION:
+		productStr = "4"
+	}
+	return clt.baseClient.Get("/positions", map[string]string{"product": productStr, "side": sideStr})
 }
