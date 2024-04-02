67fa14f7524618884b968205478c88e82abb97ce
Alice <you@example.com>
2024-04-02 17:28:21 +0900

    return decoded response

--- info/symbolnameoptionget/symbolnameoptionget.go
+++ info/symbolnameoptionget/symbolnameoptionget.go
@@ -1,13 +1,15 @@
 package symbolnameoptionget
 
 import (
+	"encoding/json"
+	"fmt"
 	"strconv"
 
 	"github.com/hsmtkk/aukabucomgo/base"
 )
 
 type Client interface {
-	SymbolNameOptionGet(optionCode OptionCode, derivMonth int, putOrCall PutOrCall, strikePrice int) ([]byte, error)
+	SymbolNameOptionGet(optionCode OptionCode, derivMonth int, putOrCall PutOrCall, strikePrice int) (ResponseSchema, error)
 }
 
 func New(baseClient base.Client) Client {
@@ -32,7 +34,12 @@ const (
 	CALL
 )
 
-func (clt *clientImpl) SymbolNameOptionGet(optionCode OptionCode, derivMonth int, putOrCall PutOrCall, strikePrice int) ([]byte, error) {
+type ResponseSchema struct {
+	Symbol     string `json:"Symbol"`
+	SymbolName string `json:"SymbolName"`
+}
+
+func (clt *clientImpl) SymbolNameOptionGet(optionCode OptionCode, derivMonth int, putOrCall PutOrCall, strikePrice int) (ResponseSchema, error) {
 	optionCodeMap := map[OptionCode]string{
 		NK225op:     "NK225op",
 		NK225miniop: "NK225miniop",
@@ -43,5 +50,13 @@ func (clt *clientImpl) SymbolNameOptionGet(optionCode OptionCode, derivMonth int
 		CALL: "C",
 	}
 	putOrCallStr := putOrCallMap[putOrCall]
-	return clt.baseClient.Get("/symbolname/future", map[string]string{"OptionCode": optionCodeStr, "DerivMonth": strconv.Itoa(derivMonth), "PutOrCall": putOrCallStr, "StrikePrice": strconv.Itoa(strikePrice)})
+	respBytes, err := clt.baseClient.Get("/symbolname/future", map[string]string{"OptionCode": optionCodeStr, "DerivMonth": strconv.Itoa(derivMonth), "PutOrCall": putOrCallStr, "StrikePrice": strconv.Itoa(strikePrice)})
+	if err != nil {
+		return ResponseSchema{}, err
+	}
+	decoded := ResponseSchema{}
+	if err := json.Unmarshal(respBytes, &decoded); err != nil {
+		return ResponseSchema{}, fmt.Errorf("failed to unmarshal JSON: %w", err)
+	}
+	return decoded, nil
 }
